d73561390e52335bc82091254d88ee4493edc25a
Himanshu2233 <sahkishor24@gmail.com>
2026-01-06 14:28:50 +0545

    "constants and model adjustments"

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,11 @@
+/**
+ * Routes Module - Central Export
+ * Exports all route configurations and components
+ */
+
+export { publicRoutes } from './publicRoutes';
+export { authRoutes } from './authRoutes';
+export { privateRoutes } from './privateRoutes';
+export { adminRoutes } from './adminRoutes';
+export { NAV_CONFIG, ROUTE_PATHS, ROUTE_METADATA } from './constants';
+export { AppRoutes } from './AppRoutes';
